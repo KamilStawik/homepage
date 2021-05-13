80808ec01b1b84f0d9f1481b887efee34f8fbe87
Kamil Stawik <kamil_stawik@op.pl>
2021-05-13 16:42:35 +0200

    js function reworked

--- js/script.js
+++ js/script.js
@@ -1,44 +1,30 @@
-console.log("Hello World");
-
-
-let buttonRemovingHeading = document.querySelector(".jsButtonRemovingHeading");
-let mainHeading = document.querySelector(".webSiteTitle");
-
-let buttonChangingBackground = document.querySelector(".jsButtonChangingBackground");
-let interestingParagraph = document.querySelector(".article");
-
-let buttonNightReading = document.querySelector(".jsButtonNightReading");
-
-
-buttonRemovingHeading.addEventListener("click", () => {
-    mainHeading.classList.toggle("webSiteTitle--none");
-    buttonRemovingHeading.innerText === "Usuń Nagłówek" ? buttonRemovingHeading.innerText = "Pokaż nagłówek" : buttonRemovingHeading.innerText = "Usuń Nagłówek";
-});
-
-buttonChangingBackground.addEventListener("click", () => {
-    if (buttonChangingBackground.innerText === "Nie chcę dalej czytać") {
-        buttonChangingBackground.innerText = "Chcę czytać dalej";
-        interestingParagraph.classList.add("article--hiden");
-        interestingParagraph.classList.remove("article");
-        interestingParagraph.classList.remove("article--nightReading");
-    } else {
-        buttonChangingBackground.innerText = "Nie chcę dalej czytać";
-        interestingParagraph.classList.remove("article--hiden");
-        interestingParagraph.classList.remove("article--nightReading");
-        interestingParagraph.classList.add("article");
+{
+    const welcomingMessage = () => {
+        console.log("Hello World");
     }
-});
 
-buttonNightReading.addEventListener("click", () => {
-    if (buttonNightReading.innerText === "Tryb nocny") {
-        buttonNightReading.innerText = "Tryb dzienny";
-        interestingParagraph.classList.add("article--nightReading");
-        interestingParagraph.classList.remove("article");
-        interestingParagraph.classList.remove("article--hiden");
-    } else {
-        buttonNightReading.innerText = "Tryb nocny";
-        interestingParagraph.classList.remove("article--nightReading");
-        interestingParagraph.classList.remove("article--hiden");
-        interestingParagraph.classList.add("article");
+    welcomingMessage();
+
+    const removingHeading = () => {
+        const mainHeading = document.querySelector(".js-webSiteTitle");
+        const buttonRemovingHeading = document.querySelector(".js-buttonRemovingHeading");
+        mainHeading.classList.toggle("webSiteTitle--none");
+        buttonRemovingHeading.innerText === "Usuń Nagłówek" ? buttonRemovingHeading.innerText = "Pokaż nagłówek" : buttonRemovingHeading.innerText = "Usuń Nagłówek";
     }
-});
+
+    const onNightReadingMode = () => {
+        const buttonNightReading = document.querySelector(".js-buttonNightReading");
+        const article = document.querySelector(".js-article");
+        article.classList.toggle("article--nightReading");
+        buttonNightReading.innerText === "Tryb nocny" ? buttonNightReading.innerText = "Tryb dzienny" : buttonNightReading.innerText = "Tryb nocny";
+    }
+
+    const init = () => {
+        const buttonRemovingHeading = document.querySelector(".js-buttonRemovingHeading");
+        buttonRemovingHeading.addEventListener("click", () => removingHeading());
+        const buttonNightReading = document.querySelector(".js-buttonNightReading");
+        buttonNightReading.addEventListener("click", () => onNightReadingMode());
+    }
+
+    init();
+}
